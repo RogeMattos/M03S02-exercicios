09a91348ae0b36b593a51fd73808485afe159568
RogeMattos <rogemattos.rm@gmail.com>
2023-08-10 20:07:50 -0300

    Configuração connection String

--- FichaCadastroSln/FichaCadastroApi/Program.cs
+++ FichaCadastroSln/FichaCadastroApi/Program.cs
@@ -9,9 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+//conection string
 string connectionString = "Server=localhost;Database=FichaCadastro;Trusted_Connection=True;TrustServerCertificate=True;";
-
+//
 builder.Services.AddDbContext<FichaCadastroDbContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
