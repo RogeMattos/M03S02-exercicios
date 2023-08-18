e1c1c1841fbe732a74291549d5c31b0cb7b8e7a6
RogeMattos <rogemattos.rm@gmail.com>
2023-08-18 16:57:57 -0300

    [M3S02] Ex 03 - Configurar o Contexto com a Model

--- FichaCadastroSln/FichaCadastroApi/Model/FichaCadastroContextDb.cs
+++ FichaCadastroSln/FichaCadastroApi/Model/FichaCadastroContextDb.cs
@@ -11,23 +11,24 @@ namespace FichaCadastroApi.Model
         public DbSet<FichaModel> FichaModels { get; set; }
         public DbSet<DetalheModel> DetalheModels { get; set; }
 
+        //[M3S02] Ex 03 - Configurar o Contexto com a Model
         public DbSet<TelefoneModel> TelefoneModels { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
             modelBuilder.Entity<DetalheModel>()
                        .HasOne(e => e.Ficha)
                        .WithMany(x => x.Detalhes)
                        .Metadata
                        .DeleteBehavior = DeleteBehavior.Restrict;
 
+            //relacionamento configurado
+
             modelBuilder.Entity<TelefoneModel>()
                 .HasOne(e => e.Ficha)
                 .WithMany(x => x.Telefones)
                 .Metadata
                 .DeleteBehavior = DeleteBehavior.Restrict;
-
 
             base.OnModelCreating(modelBuilder);
         }
