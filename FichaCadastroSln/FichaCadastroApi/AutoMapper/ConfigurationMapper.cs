02063c0d7b5f4bd3a514943bd2cdc0ea106e99a3
RogeMattos <rogemattos.rm@gmail.com>
2023-08-20 09:24:02 -0300

    [M3S02] Ex 02 - Crie uma Dto chamada Telefone

--- FichaCadastroSln/FichaCadastroApi/AutoMapper/ConfigurationMapper.cs
+++ FichaCadastroSln/FichaCadastroApi/AutoMapper/ConfigurationMapper.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using FichaCadastroApi.DTO.Ficha;
+using FichaCadastroApi.DTO.Telefone;
 using FichaCadastroApi.Model;
 
 namespace FichaCadastroApi.AutoMapper
@@ -16,9 +17,28 @@ namespace FichaCadastroApi.AutoMapper
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.EmailInformado.ToLower()))
                 .ForMember(dest => dest.DataNascimento, opt => opt.MapFrom(src => src.DataDeNascimento));
 
+            //Configuração da maper para a tabela telefone
+            CreateMap<FichaModel, TelefoneModel>()
+                .ForMember(dest => dest.Ficha, opt => opt.MapFrom(src => src.Telefones));
+            
+            CreateMap<TelefoneCreateDTO, TelefoneModel>()
+              .ForMember(dest => dest.Ddd, opt => opt.MapFrom(src => src.Ddd))
+               .ForMember(dest => dest.Numero, opt => opt.MapFrom(src => src.Numero))
+               .ForMember(dest => dest.Ativo, opt => opt.MapFrom(src => src.Ativo));
+
+            // [M3S02] Ex 02 - Configurar o Automapper para concatenar o DDD + Telefone
+            CreateMap<TelefoneModel, TelefoneReadDTO>()
+            .ForMember(dest => dest.Contato, opt => opt.MapFrom(src => $"{src.Ddd} + {src.Numero}"));
+
             CreateMap<FichaUpDateDTO, FichaModel>();
 
             CreateMap<DetalheModel, FichaDetalheReadDTO>();
+
+            CreateMap<TelefoneUpDateDTO, TelefoneModel>();
+
+            
+
+
         }
     }
 }
---
+++ FichaCadastroSln/FichaCadastroApi/DTO/Telefone/TelefoneReadDTO.cs
@@ -0,0 +1,15 @@
+﻿using FichaCadastroApi.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace FichaCadastroApi.DTO.Telefone
+{
+    //[M3S02] Ex 02 - Crie uma Dto chamada Telefone
+    public class TelefoneReadDTO : DTOBase
+    {
+        // [M3S02] Ex 02 - Criar a propriedade que devolve a lista de telefone
+        public string Contato { get; set; }
+ 
+        public bool Ativo { get; set; }
+
+    }
+}
